5872b9a42ddd91c1648e4c4aa1bf67bb56f45f5f
JPoag <James.Poag@gmail.com>
2018-09-10 12:33:01 -0400

    comments

--- decompress.js
+++ decompress.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const zlib = require('zlib');
 
+// loops through ./output folder and unzips all files
+// to ./unzipped folder
+
 async function decompress() {
     fs.mkdir('./unzipped', () => { });
 
@@ -10,18 +13,24 @@ async function decompress() {
             if (err) {
                 reject_fs(new Error(err));
             } else {
-                const write_promises = files.map((file) => {
-                    const file_name = file.split(".");
 
+                // map all the files to promises
+                const write_promises = files.map((file) => {
+
+                    // pop the 'gz' from the end of the filename
+                    const file_name = file.split(".");
                     file_name.pop();
+
+                    // create paths
                     const out_file_path = path.join('./unzipped', file_name.join("."));
                     const in_file_path = path.join('./output', file);
 
+                    // pipe the read through GUnzp and out to write
                     const rstrm = fs.createReadStream(in_file_path);
                     const wstrm = fs.createWriteStream(out_file_path);
                     const stream = rstrm.pipe(zlib.createGunzip()).pipe(wstrm);
 
-
+                    // wait on the stream to finish writing
                     return new Promise((resolve) => {
                         stream.on('close', () => {
                             console.log(`Unzipped ${in_file_path} => ${out_file_path}`)
@@ -32,9 +41,10 @@ async function decompress() {
                     });
                 })
 
+                // wait on the map'd promises to resolve
                 await Promise.all(write_promises);
 
-                resolve_fs();
+                resolve_fs(); // let outer promise resolve
             }
         });
     });
