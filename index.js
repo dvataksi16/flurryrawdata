a5bc8c1794440b1915e39a42366116160445672b
JPoag <James.Poag@gmail.com>
2018-09-10 08:55:29 -0400

    first download of job result

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const config = {
     token: "~~YOUR TOKEN~~"
 }; //*/
 
+var download_queue = [];
+var waiting_job_queue = [];
+var failed_job_queue = [];
+
 const config = require('./config') // for security reasons, I have put tokens in separate file
 
 function createJobRequestObject(start_time, period) {
@@ -46,7 +50,18 @@ function createJobRequest(start_time, period) {
     };
 }
 
-//rp(createJobRequest(config.start_time, config.period)).then(console.log).catch(console.log)
+function createJob(start_time, period) {
+    return rp(createJobRequest(start_time, period))
+        .then(data => {
+            let data_obj = JSON.parse(data);
+            if (data_obj && data_obj.data && data_obj.data.id) {
+                waiting_job_queue.push(data_obj.data.id)
+            }
+        })
+        .catch(console.log)
+}
+
+//
 
 const jobid = '20950';
 
@@ -65,4 +80,38 @@ function createPollRequest(job_id) {
     };
 }
 
-rp(createPollRequest(jobid)).then(console.log).catch(console.log)
+function checkJobStatus(job_id) {
+    return rp(createPollRequest(job_id))
+        .then(data => {
+            let data_obj = JSON.parse(data);
+            if (data_obj.data.attributes.requestStatus == "Success") {
+                waiting_job_queue = waiting_job_queue.filter(j => j != job_id);
+                download_queue.push(data_obj.data.attributes.s3URI);
+            }
+        })
+        .catch(console.log)
+}
+
+function download(uri, filename) {
+    var ext = config.format.toLocaleLowerCase() + ".gz";
+
+    return rp.get(uri)
+        .pipe(fs.createWriteStream(`${filename}.${ext}`));
+};
+
+async function main() {
+    await checkJobStatus(jobid);
+
+    try {
+        fs.mkdir('./output', err => {});
+    }
+    catch (e) {  }
+
+    let downloading_promises = download_queue.map(uri => {
+        download(uri, "./output/test")
+    })
+
+    await Promise.all(downloading_promises)
+}
+
+main()
